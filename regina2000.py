8f189e8e559151705ca39f42f1629e05c2137c86
Avi Keinan <37156629+Burekasim@users.noreply.github.com>
2023-09-15 02:06:48 +0300

    Update regina2000.py
    Use natural sort instead of sorted

--- regina2000.py
+++ regina2000.py
@@ -1,6 +1,14 @@
 import datetime
 from PyPDF2 import PdfFileMerger
 import os
+import re
+
+
+def natural_sort(l):
+    # use natural sort instead of sorted, taken from https://stackoverflow.com/questions/4836710/is-there-a-built-in-function-for-string-natural-sort
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    return sorted(l, key=alphanum_key)
 
 
 def get_pdf_files_from_current_folder(current_folder: str):
@@ -8,7 +16,7 @@ def get_pdf_files_from_current_folder(current_folder: str):
     for item in os.listdir(current_folder):
         if item.endswith('.pdf'):
             output_list.append(item)
-    return sorted(output_list)
+    return natural_sort(output_list)
 
 
 if __name__ == '__main__':
